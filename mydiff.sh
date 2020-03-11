da0fba7219f64f0e0bc9571d9e57b77b5afd665a
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2020-03-11 17:19:50 +0100

    the test should know it's 2020 now

--- mydiff.sh
+++ mydiff.sh
@@ -4,7 +4,7 @@ if test -e $1
 then
   if test -e $2
   then
-      diff -w -B --ignore-matching-lines=".?*, terminated at .?*" --ignore-matching-lines=".?*wrote PID.?*" --ignore-matching-lines=".?*case base[,: ].*" --ignore-matching-lines=".?*Algorithm.?*" --ignore-matching-lines=".?*Processing data?*" --ignore-matching-lines=".?* intermediate file.*" $1 $2 | grep -v "201[0123456789]" |grep -v "\-\-\-" | grep -v "[0-9]\+[ac][0-9]\+" | grep -v "took [0-9]* seconds" | grep -v "Seconds taken:" | grep -v "based on" | grep -v "cannot remove " > md.tmp 2>& 1
+      diff -w -B --ignore-matching-lines=".?*, terminated at .?*" --ignore-matching-lines=".?*wrote PID.?*" --ignore-matching-lines=".?*case base[,: ].*" --ignore-matching-lines=".?*Algorithm.?*" --ignore-matching-lines=".?*Processing data?*" --ignore-matching-lines=".?* intermediate file.*" $1 $2 | grep -v "20[12][0123456789]" |grep -v "\-\-\-" | grep -v "[0-9]\+[ac][0-9]\+" | grep -v "took [0-9]* seconds" | grep -v "Seconds taken:" | grep -v "based on" | grep -v "cannot remove " > md.tmp 2>& 1
       exit `wc -c < md.tmp`
   else
       echo
